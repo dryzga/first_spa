65c3ff701e8f91954828ea3de3c9560a7d986b79
Dawid Drzala <dryzga@dryzga-VirtualBox.(none)>
2012-11-03 10:23:20 +0100

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstSpa::Application.config.secret_token = '088769b439bfc9a0bc8dc8fdda00c24c46583c44eb0065c9e13cef86685ee6dd9e123e1ac5b09731005411ec67eeec53b015e87aa055e094d30464578fdecddf'
